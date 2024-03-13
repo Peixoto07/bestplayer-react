487436992944719d74562144a845e964b0cd30a0
Peixoto07 <lucaspeixotodacruz@gmail.com>
2024-03-13 20:46:36 -0300

    cria lista de jogadores

--- src/components/PlayerList/PlayerList.jsx
+++ src/components/PlayerList/PlayerList.jsx
@@ -2,29 +2,79 @@
 import './PlayerList.css'
 import { TiDelete } from "react-icons/ti";
 import { MdEdit } from "react-icons/md";
+import { GiSoccerKick } from "react-icons/gi";
+import EditPlayerModal from "../EditPlayerModal/EditPlayerModal";
+import { useState } from 'react';
 
 
-const PlayerList = ({ jogadores, excluirJogador }) => {
 
-  
+
+const PlayerList = ({ jogadores, excluirJogador,atualizaJogador }) => {
+
+  const [modalStatus, setModalStatus] = useState(false);
+  const [selectedPlayer, setSelectedPlayer] = useState(null);
+
+  const handleEditClick = (jogador) => {
+    const playerData = jogador;
+    setSelectedPlayer(playerData); 
+    handleModal();
+  };
+
+  const handleModal = () => {
+    setModalStatus((modalStatus) => !modalStatus);
+
+  };
+
+
+  const textoListaCheia = () => {
     return (
+      <div className='textoLista'>
+        <h3>Jogadores</h3>
+        <span className='quantidadeJogador'>{jogadores.length}</span>
+      </div>
+    )
+  }
+
+  const textoListaVazia = () => {
+    return (
+      <div>
+        <h3>Cadastre um jogador</h3>
+      </div>
+    )
+  }
+
+  return (
     <div className="listContainer">
-     { console.log(jogadores)}
-      <h2>Lista de Jogadores</h2>
-      <ul>
-        {jogadores.map((jogador, index) => (
-          <li key={index}>
-            <div className="divNameList">
-              <strong>{jogador.nome}</strong>
-            </div>
-            <div className="divIcons">
-              <TiDelete onClick={() => excluirJogador(index)} className="iconList" />
-              <MdEdit className="iconList" />
-            </div>
-          </li>
-        ))}
-      </ul>
+
+      {jogadores.length === 0 ? textoListaVazia() : textoListaCheia()}
+
+      {jogadores.length === 0 ? (
+        <GiSoccerKick className='soccerIcon' />
+      ) : (
+        <ul>
+          {jogadores.map((jogador, index) => (
+            <li key={index}>
+              <div className="divNameList">
+                <strong>{jogador.nome}</strong>
+              </div>
+              <div className="divIcons">
+                <TiDelete onClick={() => excluirJogador(jogador.id)} className="iconList" />
+                <MdEdit onClick={() => handleEditClick(jogador)} className="iconList" />
+
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+      {modalStatus === true ? (
+        <EditPlayerModal
+          jogadorSelecionado={selectedPlayer}
+          statusModal={handleModal}
+          atualizaJogador={atualizaJogador}
+        />
+      ) : ("")}
     </div>
+
   );
 };
 
