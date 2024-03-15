b7ca8fbd32f1fcf5cfc3bb068c1e474ceb9b75aa
Peixoto07 <lucaspeixotodacruz@gmail.com>
2024-03-15 20:00:24 -0300

    altera o componente Alert para o Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,28 @@
 import './Header.css'
 import { FiMenu } from "react-icons/fi";
 import { FaCircleUser } from "react-icons/fa6";
+import useAlert from '../hooks/useAlert';
+import Alert from '../Alert/Alert';
 
 
 const Header = () => {
-    return (
 
+    const { alertStatus, mensagemAlerta, cor } = useAlert();
+    return (
+        <>
+        
+        
+        {alertStatus && (
+            <Alert cor={cor}>
+                {mensagemAlerta}
+            </Alert>
+        )}
         <header className='headerContainer'>
             <FiMenu className='icon'/>
             <h3>BestPlayer</h3>
             <FaCircleUser className='icon' />
         </header>
+        </>
      );
 }
  
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,15 @@
-
 import Register from "../components/Register/Register";
 import PlayerList from "../components/PlayerList/PlayerList";
-import { useJogadores } from "../components/hooks/useJogadores";
-import Alert from "../components/Alert/Alert";
-import { useState } from "react";
+
 
 const Home = () => {
-    const [mostrarAlerta, setMostrarAlerta] = useState(false);
-    const { jogadores, adicionarJogador, atualizaJogador, excluirJogador } = useJogadores();
-    
-    const handleMostrarAlerta = (duracao) => {
-        setMostrarAlerta(true);
-        setTimeout(() => {
-          setMostrarAlerta(false);
-        }, duracao);
-      };
+   
     
     return (
         <>
-            <Alert mostrar={mostrarAlerta} cor="verde">
-                jogador atualizado
-            </Alert>
-            <Register adicionarJogador={adicionarJogador} />
-
-            <PlayerList handleMostrarAlerta ={handleMostrarAlerta} jogadores={jogadores} atualizaJogador={atualizaJogador} excluirJogador={excluirJogador} />
+            
+            <Register />
+            <PlayerList />
         </>
     );
 };
